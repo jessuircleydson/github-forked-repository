556e27a65fa14737aecb193997f509817a1f71c0
Jessuir Cleydson <jessuircleydson@gmail.com>
2017-11-19 17:08:12 -0300

    Refactoring and resolving #3

--- app/components/List.js
+++ app/components/List.js
@@ -2,46 +2,40 @@ import React from 'react';
 
 const List = ({list, type}) => {
 
-  const typeList = {
-      fork: 'fork',
-      issue: 'issue'
+  const forkedList = (item) =>  (
+    <li key={item}>
+      <a href={item.owner.html_url} target="_blank">
+        <img src={item.owner.avatar_url} width="50px"/>
+        {item.owner.login}
+      </a>
+    </li>
+  )
+  
+  const issueList = (item) => (
+    <li key={item.id}>
+      Title:{item.title}<br />
+      Created by: 
+      <a href={item.user.html_url} target="_blank"><img src={item.user.avatar_url} width="32px"/> {item.user.login}</a><br/>
+      State: {item.state}<br />
+      <a href={item.html_url} target="_blank">Abrir issue</a>
+    </li>        
+  )
+
+  const listTypes = {
+      fork: forkedList,
+      issue: issueList
   }
 
-  const forkList  = (item) => {
-    return (
-      <li key={item}>
-        <a href={item.owner.html_url} target="_blank">
-          <img src={item.owner.avatar_url} width="50px"/>
-          {item.owner.login}
-        </a>
-      </li>
-    )
-  }
-
-  const issueList = (item) => {
-    return (
-      <li key={item.id}>
-          Title:{item.title}<br />
-          Created by: 
-            <a href={item.user.html_url} target="_blank"><img src={item.user.avatar_url} width="32px"/> {item.user.login}</a><br/>
-          State: {item.state}<br />
-         <a href={item.html_url} target="_blank">Abrir issue</a>
-      </li>        
-    )
-  }
-
-  const verifyList = (item) => {
-    if(typeList[type] === "fork") {
-      return forkList(item)
-    } else if(typeList[type] === "issue") {
-        return issueList(item)
+  const getListType = (item) => {
+    if(listTypes[type]) {
+      return listTypes[type](item)
     }
   }
 
   return (
     <ul>
       {
-        list.map(verifyList)
+        list.map(getListType)
       }
     </ul>
   )
